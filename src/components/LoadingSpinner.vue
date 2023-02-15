348c766d4f624f8e9187ba28f7f695e4d9e9ae2d
wooinwoo <wooin1001@gmail.com>
2023-02-15 10:39:32 +0900

    add : LoadingSpinner

---
+++ src/components/LoadingSpinner.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="spinner-container">
+    <div class="spinner" />
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped>
+.spinner-container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 240px;
+}
+
+.spinner {
+  position: relative;
+  width: 50px;
+  height: 50px;
+  border-radius: 50%;
+  border: 5px solid #e0e0e0;
+  border-bottom: 5px solid #fe9616;
+  animation: spin 1s linear infinite;
+}
+
+@keyframes spin {
+  from {
+    transform: rotate(0deg);
+  }
+  to {
+    transform: rotate(360deg);
+  }
+}
+</style>
--- src/components/triggerObserver.vue
+++ src/components/triggerObserver.vue
@@ -1,5 +1,7 @@
 <script lang="ts">
 import { defineComponent, onMounted, onUnmounted, ref } from "vue";
+import LoadingSpinner from "@/components/LoadingSpinner.vue";
+
 export default defineComponent({
   setup(props, { emit }) {
     const trigger = ref();
@@ -39,12 +41,36 @@ export default defineComponent({
 </script>
 
 <template>
-  <div ref="trigger" class="loading">@로딩 중@</div>
+  <!-- <div ref="trigger" class="loading">@로딩 중@</div> -->
+  <div ref="trigger" class="spinner-container">
+    <div class="spinner" />
+  </div>
 </template>
 
 <style scoped>
-.loading {
+.spinner-container {
   display: flex;
   justify-content: center;
+  align-items: center;
+  height: 100px;
+}
+
+.spinner {
+  position: relative;
+  width: 30px;
+  height: 30px;
+  border-radius: 50%;
+  border: 5px solid #e0e0e0;
+  border-bottom: 5px solid gray;
+  animation: spin 1s linear infinite;
+}
+
+@keyframes spin {
+  from {
+    transform: rotate(0deg);
+  }
+  to {
+    transform: rotate(360deg);
+  }
 }
 </style>
