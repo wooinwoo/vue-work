848e52f56ac74f4c2c3d904aaef655c317e07db0
wooinwoo <wooin1001@gmail.com>
2023-02-15 10:44:04 +0900

    add : typings.d.ts

---
+++ typings/typings.d.ts
@@ -0,0 +1,11 @@
+declare module "@ckeditor/ckeditor5-vue" {
+  const ClassicEditorBuild: any;
+
+  export = ClassicEditorBuild;
+}
+
+declare module "ckeditor5-custom-build/build/ckeditor" {
+  const ClassicEditorBuild: any;
+
+  export = ClassicEditorBuild;
+}
